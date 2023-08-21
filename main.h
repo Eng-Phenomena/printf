86bbc82e7c81a7dd6e868231064d3ea7416b8d39
Eng-Phenomena <eng.phenomena@gmail.com>
2023-08-21 06:16:15 -0700

    ch

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
 int _printf(const char *format, ...);
 int _strlen(const char *str);
 int _putchar(const char c);
